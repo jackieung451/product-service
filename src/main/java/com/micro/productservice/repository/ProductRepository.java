7210d51bacf57b935b4e634e87cdad210d31df19
jackieung451 <jackieung451@gmail.com>
2021-09-17 16:06:18 -0700

    Updated ConfigServer and Bootstrap Properties File and TestController

---
+++ src/main/java/com/micro/productservice/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package com.micro.productservice.repository;
+
+import com.micro.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+
+}
